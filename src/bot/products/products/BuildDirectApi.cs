04b3a04956a642c36018b2b62f486e0da5e977f0
Anna Zietlow <azietlow@pnimedia.com>
2016-09-17 15:38:10 -0700

    clean

--- src/bot/products/products/BuildDirectApi.cs
+++ src/bot/products/products/BuildDirectApi.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Script.Serialization;
-using Newtonsoft.Json;
 using products.Models;
 
 namespace products
