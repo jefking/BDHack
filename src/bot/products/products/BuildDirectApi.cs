a165577d38541ac66b268c07895da633ed249faa
Anna Zietlow <azietlow@pnimedia.com>
2016-09-17 15:35:38 -0700

    bd search

---
+++ src/bot/products/products/BuildDirectApi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using products.Models;
+
+namespace products
+{
+    public class BuildDirectApi
+    {
+        public async Task<IEnumerable<SearchProduct>>  GetProducts(string term, int? page = 1, int? pageSize = 100)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "4df1c1eabd384ddab759ca6cad9e5a14");
+
+            // Request parameters
+            queryString["page"] = page.ToString();
+            queryString["pagesize"] = pageSize.ToString();
+            var uri = "https://api.builddirect.io/products/?query=" + term + "&" + queryString;
+
+            var response = await client.GetAsync(uri);
+            var json = await response.Content.ReadAsStringAsync();
+            var searchResult = new JavaScriptSerializer().Deserialize<SearchResult>(json);
+            return searchResult.Data.Products;
+        }
+    }
+}
---
+++ src/bot/products/products/Models/SearchResult.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace products.Models
+{
+    public class SearchResult
+    {
+        public SearchData Data { get; set; }
+    }
+
+    public class SearchData
+    {
+        public int TotalRecordCount { get; set; }
+        public string Query { get; set; }
+        public string OriginalQuery { get; set; }
+        public PageInfo PageInfo { get; set; }
+        public IEnumerable<Navigation> AvailableNavigations {get; set;}
+        public IEnumerable<SearchProduct> Products { get; set; }
+    }
+
+    public class PageInfo
+    {
+        public int RecordStart { get; set; }
+        public int RecordEnd { get; set; }
+        public int TotalRecordCount { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+
+    public class Navigation
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public IEnumerable<Refinement> Refinements { get; set; }
+    }
+
+    public class Refinement
+    {
+        public string Type { get; set; }
+        public int Count { get; set; }
+        public string Value { get; set; }
+        public bool HasResults { get; set; }
+    }
+
+    public class SearchProduct
+    {
+        public Brand Brand { get; set; }
+        public Category Category { get; set; }
+        public float Comparable { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Brand
+    {
+        public string Name { get; set; }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+    }
+}
