b31fa186362c09bd3121c0dfb1553e623afa8dcb
Jef King <jefkin@microsoft.com>
2016-09-17 15:26:43 -0700

    clear

--- src/bot/products/products/Controllers/MessagesController.cs
+++ src/bot/products/products/Controllers/MessagesController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Net;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Dialogs;
 
 namespace products
 {
@@ -16,30 +11,6 @@ namespace products
     
     public class MessagesController : ApiController
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
-        //public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
-        //{
-        //    if (activity.Type == ActivityTypes.Message)
-        //    {
-        //        ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-        //        // calculate something for us to return
-        //        int length = (activity.Text ?? string.Empty).Length;
-
-        //        // return our reply to the user
-        //        Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters...");
-        //        await connector.Conversations.ReplyToActivityAsync(reply);
-        //    }
-        //    else
-        //    {
-        //        HandleSystemMessage(activity);
-        //    }
-        //    var response = Request.CreateResponse(HttpStatusCode.OK);
-        //    return response;
-        //}
-
         [ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
