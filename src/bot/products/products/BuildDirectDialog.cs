6dc47de05d64e485b85ec9c4a81fee256eb7761c
Jef King <jefkin@microsoft.com>
2016-09-17 17:57:37 -0700

    bad me

--- src/bot/products/products/BuildDirectDialog.cs
+++ src/bot/products/products/BuildDirectDialog.cs
@@ -147,16 +147,6 @@
 
         #region Fun
         [LuisIntent("Thanks")]
-        public async Task Thank(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync("No... THANK YOU! (you are now my best friend).");
-
-            context.Wait(MessageReceived);
-        }
-        #endregion
-
-        #region Fun
-        [LuisIntent("Thanks")]
         public async Task Thanks(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("No... THANK YOU! (you are now my best friend).");
